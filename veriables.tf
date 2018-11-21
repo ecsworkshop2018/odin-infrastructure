7ff163814188e4970a3d3727b28177fc36029b1f
Prashant Kalkar <kalkar.prashant@gmail.com>
2018-11-21 17:14:16 +0530

    Updated docker image version.

--- veriables.tf
+++ veriables.tf
@@ -1,3 +1,3 @@
 variable "docker_image" {
-  default = "738035286324.dkr.ecr.us-east-1.amazonaws.com/prashant/odin:63ee3b828def8bba35d79ee016816d81879a276c"
+  default = "738035286324.dkr.ecr.us-east-1.amazonaws.com/prashant/odin:051e8a854e90ab80b31d8c495d2ea010e50c472e"
 }
