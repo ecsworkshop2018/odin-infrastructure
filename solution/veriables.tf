de0f154fad34f36d63657b771813b7db0a83e9a0
Prashant Kalkar <kalkar.prashant@gmail.com>
2018-12-08 03:28:15 +0530

    Removed the image link from the code to make the code fail.

--- solution/veriables.tf
+++ solution/veriables.tf
@@ -1,3 +1,3 @@
 variable "docker_image" {
-  default = "738035286324.dkr.ecr.us-east-1.amazonaws.com/prashant/odin:051e8a854e90ab80b31d8c495d2ea010e50c472e"
+//  default = "738035286324.dkr.ecr.us-east-1.amazonaws.com/prashant/odin:051e8a854e90ab80b31d8c495d2ea010e50c472e"
 }
